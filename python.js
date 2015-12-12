e4577ffb3e89c81ca1ebb653402abdabc0b42d9d
Lennart Kats <lclkats@gmail.com>
2015-12-12 15:54:37 +0000

    Move jsonalyzer python plugins into c9.ide.language.python

--- python.js
+++ python.js
@@ -1,18 +1,28 @@
 /**
- * Cloud9 python support
+ * Cloud9 Python support
  *
  * @copyright 2015, Ajax.org B.V.
  */
 define(function(require, exports, module) {
-    main.consumes = ["language.python"];
-    main.provides = [];
+    main.consumes = ["language", "jsonalyzer"];
+    main.provides = ["language.python"];
     return main;
 
     function main(options, imports, register) {
+        var Plugin = imports.Plugin;
         var language = imports.language;
-
-        language.registerLanguageHandler("plugins/c9.ide.language.python/worker/python_worker");
+        var jsonalyzer = imports["jsonalyzer"];
+        var plugin = new Plugin("Ajax.org", main.consumes);
         
-        register(null, {});
+        plugin.on("load", function() {
+            language.registerLanguageHandler("plugins/c9.ide.language.python/worker/python_worker");
+            jsonalyzer.registerWorkerHandler("plugins/c9.ide.language.python/worker/python_jsonalyzer_worker");
+            jsonalyzer.registerServerHandler("plugins/c9.ide.language.python/worker/python_jsonalyzer_server_worker");
+        });
+        
+        /** @ignore */
+        register(null, {
+            "language.python": plugin
+        });
     }
 });
