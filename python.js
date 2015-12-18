a2ca0f2bb3f3a9cbcf6981da2896c279ba688e09
Lennart Kats <lclkats@gmail.com>
2015-12-18 09:30:03 +0000

    Mark as experimental for now

--- python.js
+++ python.js
@@ -4,7 +4,7 @@
  * @copyright 2015, Ajax.org B.V.
  */
 define(function(require, exports, module) {
-    main.consumes = ["Plugin", "language", "jsonalyzer"];
+    main.consumes = ["Plugin", "language", "jsonalyzer", "preferences.experimental"];
     main.provides = ["language.python"];
     return main;
 
@@ -12,10 +12,14 @@ define(function(require, exports, module) {
         var Plugin = imports.Plugin;
         var language = imports.language;
         var jsonalyzer = imports["jsonalyzer"];
+        var experimental = imports["preferences.experimental"];
         var plugin = new Plugin("Ajax.org", main.consumes);
         
+        var enabled = experimental.addExperiment("python_worker", false, "Language/Python Code Completion");;
+        
         plugin.on("load", function() {
-            language.registerLanguageHandler("plugins/c9.ide.language.python/worker/python_worker");
+            if (enabled)
+                language.registerLanguageHandler("plugins/c9.ide.language.python/worker/python_worker");
             jsonalyzer.registerWorkerHandler("plugins/c9.ide.language.python/worker/python_jsonalyzer_worker");
             jsonalyzer.registerServerHandler("plugins/c9.ide.language.python/server/python_jsonalyzer_server_worker");
         });
