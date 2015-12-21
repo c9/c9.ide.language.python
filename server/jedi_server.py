3bea7dd0a8699e1bf1cdb2a1d1befb24fcb0f8b4
Lennart Kats <lclkats@gmail.com>
2015-12-21 12:34:26 +0000

    Add daemon mode to jedi_server

--- server/jedi_server.py
+++ server/jedi_server.py
@@ -3,8 +3,50 @@ import argparse
 import jedi
 import json
 import sys
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
 
-def to_json(c, template):
+def main(args):
+    if args.mode != "daemon":
+        print(run(sys.stdin.read(), args.__dict__))
+        return
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('localhost', 7680), Daemon)
+    print 'Starting daemon at :7680'
+    server.serve_forever()
+
+def run(source, args):
+    print "run %s" % args
+    script = jedi.Script(source, int(args.get("row")), int(args.get("column")), args.get("path"))
+    try:
+        mode = args.get("mode")
+        if mode == 'completions':
+            result = script.completions()
+        elif mode == 'goto_definitions':
+            result = script.goto_definitions()
+        elif mode == 'goto_assignments':
+            result = script.goto_assignments()
+        elif mode == 'call_signatures':
+            result = script.call_signatures()
+        else:
+            raise
+    except:
+        result = []
+    return json.dumps(result, default=to_json)
+
+class Daemon(BaseHTTPRequestHandler):
+    def do_POST(self):
+        query = urlparse.urlparse(self.path).query
+        args = urlparse.parse_qsl(query)
+        
+        length = int(self.headers.getheader('content-length', 0))
+        source = self.rfile.read(length)
+        
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(run(source, dict(args)))
+
+def to_json(c):
     try:
         paramList = { p.description for p in c.params }
         params = ", ".join([p for p in paramList if p != None])
@@ -39,27 +81,12 @@ def remove_nulls(d):
 def abbrev(s):
     return s if len(s) < 2500 else s[:2500] + "..."
 
-def main(args):
-    script = jedi.Script(sys.stdin.read(), args.row, args.column)
-    try:
-        if args.function == 'completions':
-            data = script.completions()
-        if args.function == 'goto_definitions':
-            data = script.goto_definitions()
-        if args.function == 'goto_assignments':
-            data = script.goto_assignments()
-        if args.function == 'call_signatures':
-            data = script.call_signatures()
-    except:
-        data = []
-    print(json.dumps(data, default=to_json))
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run jedi functions over a script provided via stdin')
-    parser.add_argument('function', help='Which jedi function you want to call', choices=['completions', 'goto_definitions', 'goto_assignments'])
-    parser.add_argument('row', type=int, help='The row to read from')
-    parser.add_argument('column', type=int, help='The column to read from')
+    parser.add_argument('mode', help='Mode of operation', choices=['daemon', 'completions', 'goto_definitions', 'goto_assignments'])
+    parser.add_argument('--row', type=int, help='The row to read from')
+    parser.add_argument('--column', type=int, help='The column to read from')
+    parser.add_argument('--path', type=int, help='The path of the script')
 
     args = parser.parse_args()
     main(args)
