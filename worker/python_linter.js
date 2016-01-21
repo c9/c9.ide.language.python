fca9f80eda7343e94f69836ec2d22cdbdb46fc59
Lennart Kats <lclkats@gmail.com>
2016-01-21 16:24:28 +0000

    Fix pylint crashing when using custom flags
    Silly pylint

--- worker/python_linter.js
+++ worker/python_linter.js
@@ -72,7 +72,7 @@ handler.analyze = function(docValue, fullAst, options, callback) {
             }
         },
         function(err, stdout, stderr) {
-            if (err && err.code !== 2) return callback(err);
+            if (err && err.code !== 2 && err.code !== 3) return callback(err);
 
             stdout.split("\n").forEach(function(line) {
                 var marker = parseLine(line, hasStarImports);
